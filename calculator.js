124262fde8ade54d835285c36cdcee5cb4095db6
N2K-keli <kelinguma@gmail.com>
2025-09-03 21:20:42 +0100

    basic calculator with added input

--- calculator.js
+++ calculator.js
@@ -35,16 +35,29 @@ for(let i = 0; i<numberButton.length ; i++)
                         catch(error)
                         {
                             console.log("an error occured. error message : " + error)
-                            
                         }
                         // the reason i am printing ans instead of answer is just because i want to. i could very wel do
                         //screenOutput.textContent = answer . i decided to print ans instead of answer because the data type of
                         // answer is a number , so i decied to convert it to string to make sure everything on  the string is string 
                         // but if you do screenOutput.textContent = answer , JS will also convert it to a string so nothing changes.
                         // i just enjoy managing my data types my self. i think i don't know i am just a beginner so who am i to 
-                        // know what is good ir bad.
+                        // know what is good or bad.
                         
                     } 
+                else if(ButtonValue === '%')
+                {
+                    let percentageValue = eval(screenOutput.textContent)
+                    try
+                    {
+                        percentageValue = (Number(percentageValue))
+                        let ans = percentageValue / 100
+                        screenOutput.textContent =  ans
+                    }
+                    catch(error)
+                    {
+                        console.log("an error occuerd" + error.message)
+                    }
+                }
                 else
                     {
                         screenOutput.textContent = screenOutput.textContent + ButtonValue 
