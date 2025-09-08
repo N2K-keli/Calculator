09c0f52a071b0362c89f23a289a8fac9b2d82c89
N2K-keli <kelinguma@gmail.com>
2025-09-08 21:44:54 +0100

    switching from basic to scientific and vice-versa

--- calculator.js
+++ calculator.js
@@ -1,11 +1,12 @@
 let numberButton = document.getElementsByClassName("Number-Button")
-let screenOutput =  document.getElementById("Basic-Calculator-Screen-Answer")
+let screenOutput =  document.getElementsByClassName("Basic-Calculator-Screen-Answer")
 let clearButton =  document.getElementById("Clear")
 let backspaceButton = document.getElementById("Backspace");
-let previousAnswerButton =  document.getElementById("Basic-Calculator-Screen-history")
+let previousAnswerButton =  document.getElementsByClassName("Basic-Calculator-Screen-history")
 let scientificDisplay = document.getElementById("Scientifci-Calculator")
 let scientificButton = document.getElementById("Scientific-Button")
 let basicCalculatorDisplay  =  document.getElementById("Basic-Calculator")
+let basicCalculatorButton =  document.getElementById("Basic-Button")
 
 
 for(let i = 0; i<numberButton.length ; i++) 
@@ -16,29 +17,39 @@ for(let i = 0; i<numberButton.length ; i++)
                 let ButtonValue =  this.value
                 if(ButtonValue === "CE")
                     {
-                        screenOutput.textContent= ''
+                        screenOutput[0].textContent= ''
+                        screenOutput[1].textContent= ''
                     
                     }
                 else if(ButtonValue === '⌫')
                     {
-                            let screenText = screenOutput.textContent
-                            let screenTextBackspaced="";
-                            screenTextBackspaced = screenText.slice(0,screenText.length-1)
-                            screenTextBackspaced = screenTextBackspaced.trim()
-                            screenOutput.textContent = screenTextBackspaced
+                            let screenText = [screenOutput[0].textContent, screenOutput[0].textContent]
+                            let screenTextBackspaced=["",""];
+                            screenTextBackspaced[0] = screenText[0].slice(0,screenText[0].length-1)
+                            screenTextBackspaced[1] = screenText[1].slice(0,screenText[1].length-1)
+                            screenTextBackspaced[0] = screenTextBackspaced[0].trim()
+                            screenTextBackspaced[1] = screenTextBackspaced[1].trim()
+                            screenOutput[0].textContent = screenTextBackspaced[0]
+                            screenOutput[1].textContent = screenTextBackspaced[1]
                     }
                 else if(ButtonValue === '=') 
                     {
-                        let expression = screenOutput.textContent
+                        let expression = [screenOutput[0].textContent , screenOutput[0].textContent]
                         try {
-                            expression = expression.replaceAll("%","/100")
-                            expression =  expression.replaceAll("sqrt(", "Math.sqrt(")
-                            expression = expression.replaceAll("mod", "%")
-                            let answer = eval(expression)
-                            ans = String(answer)
-                            screenOutput.textContent = ans;
-                            previousAnswerButton.textContent = ans
-                            console.log(" the answer is : "+  ans + " and the type of the variable is : " + typeof(ans))
+                            expression[0] = expression[0].replaceAll("%","/100")
+                            expression[1] = expression[1].replaceAll("%","/100")
+                            expression[0] =  expression[0].replaceAll("sqrt(", "Math.sqrt(")
+                            expression[1] =  expression[1].replaceAll("sqrt(", "Math.sqrt(")
+                            expression[0] = expression[0].replaceAll("mod", "%")
+                            expression[1] = expression[1].replaceAll("mod", "%")
+                            let answer = [eval(expression[0]), eval(expression[0])]
+                            ans = [String(answer[0]),String(answer[1])]
+                            screenOutput[0].textContent = ans[0];
+                            screenOutput[1].textContent = ans[1];
+                            previousAnswerButton[0].textContent = ans[0]
+                            previousAnswerButton[1].textContent = ans[1]
+                            console.log(" the answer is : "+  ans[0] + " and the type of the variable is : " + typeof(ans[0]))
+                            console.log(" the answer is : "+  ans[1] + " and the type of the variable is : " + typeof(ans[1]))
                         }
                         catch(error)
                         {
@@ -54,11 +65,13 @@ for(let i = 0; i<numberButton.length ; i++)
                     } 
                 else if(ButtonValue === "ans") 
                     {
-                        screenOutput.textContent = screenOutput.textContent + previousAnswerButton.textContent
+                        screenOutput[0].textContent = screenOutput[0].textContent + previousAnswerButton[0].textContent
+                        screenOutput[1].textContent = screenOutput[1].textContent + previousAnswerButton[1].textContent
                     }
                 else
                     {
-                        screenOutput.textContent = screenOutput.textContent + ButtonValue 
+                        screenOutput[0].textContent = screenOutput[0].textContent + ButtonValue
+                        screenOutput[1].textContent = screenOutput[1].textContent + ButtonValue 
                     }
             });
     }
@@ -68,6 +81,11 @@ scientificButton.addEventListener('click', function(){
     
     basicCalculatorDisplay.classList.add("hidden-Elements")
     scientificDisplay.classList.remove("hidden-Elements")
+})
+basicCalculatorButton.addEventListener('click', function()
+{
+    basicCalculatorDisplay.classList.remove("hidden-Elements")
+    scientificDisplay.classList.add("hidden-Elements")
 })
 
 
